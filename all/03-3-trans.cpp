5b45ab7f8ab722c2fa1126d863f0b487908a0a6f
s0190 <gb12@mails.tsinghua.edu.cn>
2016-03-16 16:52:50 +0800

    correct

--- all/03-3-trans.cpp
+++ all/03-3-trans.cpp
@@ -24,34 +24,31 @@ void transfer(UINT va) {
     UINT pde_v = pde >> 7;
     UINT pde_c = pde & 0x0000007f;
     printf("Virtual Address 0x%04x:\n", va);
-    printf("  --> pde index:0x%x ", pd);
-    printf("pde contents:(valid %d, pfn 0x%02x)\n", pde_v, pde_c);
+    printf("  --> pde index:0x%x pde contents:(valid %d, pfn 0x%02x)\n", pd, pde_v, pde_c);
     if (pde_v == 0) {
-        cout << "      --> Fault (page directory entry not valid)" << endl;
+        printf("      --> Fault (page directory entry not valid)\n");
         return;
     }
     UINT pte_index = (va & 0x000003e0) >> 5;
     UINT pte = page[pde_c][pte_index];
     UINT pte_v = pte >> 7;
     UINT pte_c = pte & 0x0000007f;
-    printf("    --> pte index:0x%02x", pte_index);
-    printf(" pte contents:(valid %d, pfn 0x%02x)\n", pte_v, pte_c);
+    printf("    --> pte index:0x%02x pte contents:(valid %d, pfn 0x%02x)\n", pte_index, pte_v, pte_c);
     UINT page_offset = (va & 0x0000001f);
     UINT phyaddr = (pte_c << 5 )+ page_offset;
-    if (pte_v == 0) {//to disk address
+    if (pte_v == 0) { //to disk address
         UINT value = disk[pte_c][page_offset];
         printf("      --> To Disk Sector Address 0x%x -->Value: 0x%02x\n", phyaddr, value);
-        return;
-    } else {//to physical address
+    } else { //to physical address
         UINT value = getmemory(phyaddr);
         printf("      --> To Physical Address 0x%x -->Value: 0x%02x\n", phyaddr, value);
     }
 }
-void readmemory() {
+void loadmem() {
     scanf("PDBR: 0x%x\n", &pdbr);
-    UINT s;
+    UINT discard;
     for (int i=0; i<0x80; i++) {
-        scanf("page %x: ", &s);
+        scanf("page %x: ", &discard);
         for (int j=0; j<32; j++) {
             UINT temp;
             scanf("%x ", &temp);
@@ -59,22 +56,22 @@ void readmemory() {
         }
     }
     for (int i=0; i<0x80; i++) {
-        scanf("disk %x: ", &s);
+        scanf("disk %x: ", &discard);
         for (int j=0; j<32; j++) {
             UINT temp;
             scanf("%x ", &temp);
             disk[i][j] = BYTE(temp);
         }
     }
+}
+
+int main() {
+    freopen("03-3-input.txt", "r", stdin);
+    loadmem();
     UINT va;
     while (scanf("Virtual Address %x:\n", &va)!=EOF) {
         transfer(va);
         printf("\n");
     }
-}
-
-int main() {
-    freopen("03-3-input.txt", "r", stdin);
-    readmemory();
     return 0;
 }
