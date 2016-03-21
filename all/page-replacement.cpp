de465db42fff4124fda2bb391a02622123850fe4
s0190 <gb12@mails.tsinghua.edu.cn>
2016-03-21 10:14:23 +0800

    page-replacement

--- all/page-replacement.cpp
+++ all/page-replacement.cpp
@@ -1,34 +1,39 @@
 #include <cstdio>
-#include <vector>
+#include <cstring>
+
 using namespace std;
 
-#define SIZE 2
-#define TOTAL 5
+#define T 2
+#define PAGE_SIZE 5
 
-bool pages[TOTAL];
-bool use[TOTAL];
+bool pages[PAGE_SIZE];
+bool use[PAGE_SIZE];
+
 int last = -1;
+int visitNo = 0;
 
-void process(int order, int pageno) {
+void process(int pageno) {
+    ++visitNo;
     use[pageno] = true;
     if (pages[pageno]) {
         printf("Access page %d.\n", pageno);
         return;
     }
 
-    if ((last < 0) || ((order - last)<=SIZE)) {
+    if ((last < 0) || ((visitNo - last)<=T)) {
         pages[pageno] = true;
-        printf("Add into set %d.\n", pageno);
-        last = order;
+        printf("Add %d into set.\n", pageno);
+        last = visitNo;
         return;
     }
-    for (int i=0; i<TOTAL; i++) {
+    printf("page ");
+    for (int i=0; i<PAGE_SIZE; i++) {
         if (pages[i] && !use[i]) printf("%d ", i);
         pages[i] = use[i];
         use[i] = false;
     }
-    printf(" replaced.\n");
-    last = order;
+    printf("replaced.\n");
+    last = visitNo;
     return;
 }
 int main() {
@@ -37,11 +42,10 @@ int main() {
     pages[3] = true;
     pages[4] = true;
     pages[1] = pages[2] = false;
-    for (int i=0; i<TOTAL; i++) use[i] = false;
-    int pageno, order;
-    order = 0;
+    memset(use, 0, sizeof(use));
+    int pageno;
     while (scanf("%d", &pageno) != EOF) {
-        process(++order, pageno);
+        process(pageno);
     }
     return 0;
 }
